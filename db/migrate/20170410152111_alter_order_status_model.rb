dd0b4f2503f88c2e558a687b6408099742694ff2
chelsbra <cbrause09@gmail.com>
2017-04-13 04:51:40 +0000

    Edited migration files

--- db/migrate/20170410152111_alter_order_status_model.rb
+++
@@ -1,7 +0,0 @@
-class AlterOrderStatusModel < ActiveRecord::Migration[5.0]
-  def change
-  	change_table :orders do |t|
-  		t.rename :order_statu_id, :order_status_id
-  	end
-  end
-end
--- db/migrate/20170410152954_alter_orders_items_model.rb
+++
@@ -1,7 +0,0 @@
-class AlterOrdersItemsModel < ActiveRecord::Migration[5.0]
-  def change
-  	change_table :order_items do |t|
-  		t.rename :total_price, :total_price
-  end
-  end
-end
